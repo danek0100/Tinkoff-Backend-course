2c1cd80e1c13786f25c01b9648015fc2c32cec1b
danek0100 <71225718+danek0100@users.noreply.github.com>
2024-03-02 14:55:27 +0300

    HW-1 fix: useless filtration

--- bot/src/main/java/edu/java/bot/configuration/CommandConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/CommandConfig.java
@@ -3,7 +3,6 @@ package edu.java.bot.configuration;
 import edu.java.bot.command.Command;
 import edu.java.bot.command.HelpCommand;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.context.annotation.Bean;
 
 
@@ -11,9 +10,6 @@ public class CommandConfig {
 
     @Bean
     public HelpCommand helpCommand(List<Command> commands) {
-        List<Command> filteredCommands = commands.stream()
-            .filter(command -> !(command instanceof HelpCommand))
-            .collect(Collectors.toList());
-        return new HelpCommand(filteredCommands);
+        return new HelpCommand(commands);
     }
 }
